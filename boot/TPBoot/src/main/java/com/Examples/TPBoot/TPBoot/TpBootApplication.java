1f7649cc8a9785ba915a4d79caf218d4383b8b29
Kartik Reddy Sadala <ksadala@pkrmsezdc.com>
2019-01-08 13:21:26 +0530

    New Proj

---
+++ boot/TPBoot/src/main/java/com/Examples/TPBoot/TPBoot/TpBootApplication.java
@@ -0,0 +1,14 @@
+package com.Examples.TPBoot.TPBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpBootApplication.class, args);
+	}
+
+}
+
